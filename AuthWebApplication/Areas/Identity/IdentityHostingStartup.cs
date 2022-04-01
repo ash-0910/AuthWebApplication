4b8adcc8ba65a44a53b7ff9e24f0bb5596f4b083
ash-0910 <A00244471@mycambrian.ca>
2022-04-01 18:44:15 -0400

    added login and register identity razor pages

---
+++ AuthWebApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AuthWebApplication.Areas.Identity.Data;
+using AuthWebApplication.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthWebApplication.Areas.Identity.IdentityHostingStartup))]
+namespace AuthWebApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthWebApplicationContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthWebApplicationContextConnection")));
+
+                services.AddDefaultIdentity<AuthWebApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthWebApplicationContext>();
+            });
+        }
+    }
+}
--- AuthWebApplication/Startup.cs
+++ AuthWebApplication/Startup.cs
@@ -24,6 +24,8 @@ namespace AuthWebApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +45,7 @@ namespace AuthWebApplication
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,6 +53,7 @@ namespace AuthWebApplication
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
